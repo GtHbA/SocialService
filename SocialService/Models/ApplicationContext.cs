bb490818e22ed321b744da809710b6ad7ad03d1f
vislstd17 <vislstd17@yandex.ru>
2019-07-05 16:22:50 +0400

    Добавьте файлы проекта.

---
+++ SocialService/Models/ApplicationContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+
+namespace SocialService.Models
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext() : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<Friend> Friends { get; set; }
+    }
+}
